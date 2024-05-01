e64f27d1e00dd69bd5748c67c3226ece71651d39
Anh Dinh <anhdinh17@gmail.com>
2024-04-30 23:23:19 -0500

    First commit

---
+++ SwiftUIAuthTutorial/SwiftUIAuthTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIAuthTutorial
+//
+//  Created by Anh Dinh on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIAuthTutorial/SwiftUIAuthTutorial/SwiftUIAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthTutorialApp.swift
+//  SwiftUIAuthTutorial
+//
+//  Created by Anh Dinh on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
